4cd2f0b372ba74fd4e762a697ff41143da315d00
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-09-28 10:38:24 +0300

    Update ft_reverse_code.py

--- ft_reverse_code.py
+++ ft_reverse_code.py
@@ -1,15 +1,9 @@
-def ft_straight_code(a):
-    if a >= 0:
-        b = a % 2
-        while a > 0:
-            a = a // 2
-            b = b * 10 + a % 2
-        print(b)
+def ft_reverse_code(a):
     if a < 0:
-        a = a * - 1
+        a = a * -1
         b = a % 2
-        c = 10000000
         while a > 0:
             a = a // 2
             b = b * 10 + a % 2
-        print(c + b)
+            m = 11111111
+        print(m-b)
