6235660214504129b095e88be5c2ede13f3e73c1
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-10-01 11:58:53 +0300

    Update ft_reverse_code.py

--- ft_reverse_code.py
+++ ft_reverse_code.py
@@ -1,9 +1,37 @@
-def ft_reverse_code(a):
-    if a < 0:
-        a = a * -1
-        b = a % 2
-        while a > 0:
-            a = a // 2
-            b = b * 10 + a % 2
-            m = 11111111
-        print(m-b)
+def ft_len(a): 
+    l = 0 
+    for i in a: 
+        l += 1 
+    return l 
+
+
+def ft_reverse_code(x): 
+    ch = x 
+    if x > 0: 
+        n = '' 
+        while x > 0: 
+            y = f'{x % 2}' 
+            n = y + n 
+            x = int(x / 2) 
+            while ft_len(n) != 8: 
+                n = '0' + n 
+    else: 
+        x = x * -1 
+        n = '' 
+        while x > 0: 
+            y = f'{x % 2}' 
+            n = y + n 
+            x = int(x / 2) 
+            while ft_len(n) != 7: 
+                n = '0' + n 
+                n = '1' + n 
+                a = '1' 
+    if ch > 0: 
+        return n 
+    else: 
+        for i in n[1:]: 
+            if i == '0': 
+                a = a + '1' 
+            else: 
+                a = a + '0' 
+        return a
